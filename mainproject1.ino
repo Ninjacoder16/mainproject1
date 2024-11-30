177ec68f15189788d639901181b15945a1d080a1
Ninjacoder16 <mylenovo30116@gmail.com>
2024-11-30 21:46:28 +0530

    Added Gas sensor

--- mainproject1.ino
+++ mainproject1.ino
@@ -1,25 +1,27 @@
 #include <DHT.h>
 
-#define TRIG_PIN 5  // Define the GPIO pin connected to the Trigger
-#define ECHO_PIN 18 // Define the GPIO pin connected to the Echo
-// Define DHT type and GPIO pin
-#define DHTPIN 4      // GPIO pin where the DHT11 is connected
-#define DHTTYPE DHT11 // Specify DHT11 sensor type
+#define TRIG_PIN 5     // Define the GPIO pin connected to the Trigger
+#define ECHO_PIN 18    // Define the GPIO pin connected to the Echo
+#define DHTPIN 4       // GPIO pin where the DHT11 is connected
+#define DHTTYPE DHT11  // Specify DHT11 sensor type
+#define LED 13         // LED connected to digital pin 13
+#define MQ2pin A0      // MQ2 sensor connected to analog pin A0
+
 void Ualtrasonic();
-// Initialize DHT sensor
-DHT dht(DHTPIN, DHTTYPE);
+void Gas_sensor();
+
+DHT dht(DHTPIN, DHTTYPE);  // Initialize DHT sensor
 
 void setup() {
-  Serial.begin(115200); // Initialize serial communication
-  dht.begin();          // Initialize the DHT sensor
+  Serial.begin(115200);  // Initialize serial communication
+  dht.begin();           // Initialize the DHT sensor
   Serial.println("DHT11 sensor initialized.");
   Serial.println("New addded");
-  // Set the trigPin as OUTPUT and echoPin as INPUT
-  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);     // Set the trigPin as OUTPUT and echoPin as INPUT
   pinMode(ECHO_PIN, INPUT);
+  pinMode(LED, OUTPUT);  // Set the LED pin as an output
+  digitalWrite(TRIG_PIN, LOW);    // Ensure the trigPin starts LOW
 
-  // Ensure the trigPin starts LOW
-  digitalWrite(TRIG_PIN, LOW);
   delay(50);
 }
 
@@ -29,10 +31,10 @@ void loop() {
 
   // Reading temperature and humidity
   float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature(); // Celsius
+  float temperature = dht.readTemperature();  // Celsius
 
   // Check if readings are valid
- /* if (isnan(humidity) || isnan(temperature)) {
+  /* if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }*/
@@ -44,7 +46,7 @@ void loop() {
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println(" °C");
-
+  Gas_sensor();
   Ualtrasonic();
 }
 
@@ -67,4 +69,21 @@ void Ualtrasonic() {
   Serial.println(" cm");
   // Delay before taking the next reading
   delay(500);
+}
+
+void Gas_sensor() {
+  float sensorValue;                 // Variable to store the sensor value
+  sensorValue = analogRead(MQ2pin);  // Read the analog value from the MQ2 sensor
+
+  if (sensorValue >= 250) {   // Check if the sensor value exceeds the threshold
+    digitalWrite(LED, HIGH);  // Turn the LED on
+    Serial.print(sensorValue);
+    Serial.println(" | GAS DETECTED");
+  } else {
+    digitalWrite(LED, LOW);  // Turn the LED off
+    Serial.print("Sensor Value: ");
+    Serial.println(sensorValue);
+  }
+
+  delay(1000);  // Wait for 1 second
 }
