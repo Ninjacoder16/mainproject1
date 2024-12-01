d4862abf97ae4aab5452f5ea01245d04bd43dae5
Ninjacoder16 <mylenovo30116@gmail.com>
2024-12-01 23:04:59 +0530

    added lrd and gas sensor

--- mainproject1.ino
+++ mainproject1.ino
@@ -1,5 +1,5 @@
 #include <DHT.h>
-
+     
 #define TRIG_PIN 5  // Define the GPIO pin connected to the Trigger
 #define ECHO_PIN 18 // Define the GPIO pin connected to the Echo
 // Define DHT type and GPIO pin
